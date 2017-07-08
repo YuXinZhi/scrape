da3ff8c4498b5a305c8371b9101d8a5580d7992a
Administrator <whoyumengmeng@gmail.com>
2017-07-09 02:28:29 +0800

    demo 1.builtwith 2.whois 3.urllib
    note:
    1.learn method parameters
    2.html = html.decode('utf-8')

--- demo3downloadwebpage.py
+++ demo3downloadwebpage.py
@@ -1,11 +1,13 @@
 import urllib.request
+import re
 
 def download(url , user_agent = "wswp" , num_retries=2):
-    print("downloading ",url)
+    print("downloading : ",url)
     headers = {"user-agent":user_agent}
     urllib.request.Request(url,headers=headers)
     try:
         html = urllib.request.urlopen(url).read()
+        html = html.decode('utf-8')     #python2和3兼容问题
     except urllib.request.URLError as  e:
         print("download error:",e.reason)
         html = None
@@ -14,6 +16,18 @@ def download(url , user_agent = "wswp" , num_retries=2):
                 return download(url,num_retries-1)
     return html
 
-download("http://baidu.com")
+def crawl_sitemap(url):
+    #dwonload the sitemap file
+    sitemap = download(url)
+    #extract the sitemap links
+    links = re.findall('<loc>(.*?)<loc>',sitemap)
+    #downlod each lin
+    for link in links:
+        html = download(link)
+
+crawl_sitemap("http://example.webscraping.com/sitemap.xml")
+
+
+#download("http://baidu.com")
 #download("http://httpstat.us/500")
 #download("http://www.meetup.com")
